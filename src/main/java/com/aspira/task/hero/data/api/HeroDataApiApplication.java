aaab442918abc55a25567ed22b9caea4655cdc51
Serhii Ovsyuk <sergey.ovsyuk@gmail.com>
2020-05-09 12:21:59 +0300

    Added init spring-boot project

---
+++ src/main/java/com/aspira/task/hero/data/api/HeroDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.aspira.task.hero.data.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroDataApiApplication.class, args);
+	}
+
+}
